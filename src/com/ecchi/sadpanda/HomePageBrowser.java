8cbde90e8236d5d9345646f93c758dfb59365d9a
pandanon <pandanon22@gmail.com>
2013-08-19 01:52:31 +0200

    Got search to work by mixing some of my abandoned app code. Needs some clean up.

--- src/com/ecchi/sadpanda/HomePageBrowser.java
+++ src/com/ecchi/sadpanda/HomePageBrowser.java
@@ -2,14 +2,18 @@ package com.ecchi.sadpanda;
 
 import org.apache.http.cookie.Cookie;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.widget.SearchView;
 import com.ecchi.sadpanda.detailview.ImageSetDetailActivity;
 import com.ecchi.sadpanda.detailview.ImageSetDetailFragment;
 import com.ecchi.sadpanda.login.LoginFragment;
@@ -28,174 +32,206 @@ import com.novoda.imageloader.core.cache.LruBitmapCache;
 import com.novoda.imageloader.core.loader.Loader;
 
 public class HomePageBrowser extends SherlockFragmentActivity implements
-		OnLoggedinListener, OnLoginRequestListener, OnLoggedoutListener,
-		Callbacks {
+	OnLoggedinListener, OnLoginRequestListener, OnLoggedoutListener,
+	Callbacks {
 
-	private final String UA = "Exhentai Mobile/1.0";
-	public static final String Domain = ".exhentai.org";
-	public static final String URL = "http://exhentai.org/?page=";
+    private final String UA = "Exhentai Mobile/1.0";
+    public static final String Domain = ".exhentai.org";
+    public static final String BASE_URL = "http://exhentai.org/";
 
-	public static ClientWrapper CLIENT;
+    public static ClientWrapper CLIENT;
 
-	boolean isLoggedIn = false;
-	MenuItem login;
+    boolean isLoggedIn = false;
+    MenuItem login;
 
-	boolean mTwoPane = false;
+    boolean mTwoPane = false;
 
-	public static ImageManager mManager;
+    public static ImageManager mManager;
+    private String searchQuery = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_imageset_list);
+	final Intent intent = this.getIntent();
+	if (intent != null && Intent.ACTION_SEARCH.equals(intent.getAction()))
+	    searchQuery = intent.getStringExtra(SearchManager.QUERY);
 
-		LoaderSettings settings = new LoaderSettings.SettingsBuilder()
-				.withCacheManager(new LruBitmapCache(this)).build(this);
+	setContentView(R.layout.activity_imageset_list);
 
-		mManager = new ImageManager(settings);
+	final LoaderSettings settings = new LoaderSettings.SettingsBuilder()
+		.withCacheManager(new LruBitmapCache(this)).build(this);
 
-		CLIENT = ClientWrapper.newInstance(UA, this);
+	mManager = new ImageManager(settings);
 
-		if (findViewById(R.id.imageset_detail_container) != null) {
-			// The detail container view will be present only in the
-			// large-screen layouts (res/values-large and
-			// res/values-sw600dp). If this view is present, then the
-			// activity should be in two-pane mode.
-			mTwoPane = true;
-		}
+	CLIENT = ClientWrapper.newInstance(UA, this);
 
-		if (!verifyLoggedIn()) {
-			setLoggedOut();
-			login();
-		} else {
-			setLoggedIn();
-			// Load the overview fragment
-			loadFragment();
-		}
+	if (findViewById(R.id.imageset_detail_container) != null) {
+	    // The detail container view will be present only in the
+	    // large-screen layouts (res/values-large and
+	    // res/values-sw600dp). If this view is present, then the
+	    // activity should be in two-pane mode.
+	    mTwoPane = true;
 	}
 
-	public boolean isLoggedIn() {
-		return isLoggedIn;
+	if (!verifyLoggedIn()) {
+	    setLoggedOut();
+	    login();
+	} else {
+	    setLoggedIn();
+	    // Load the overview fragment
+	    loadFragment();
+	}
+    }
+
+    public boolean isLoggedIn() {
+	return isLoggedIn;
+    }
+
+    private boolean verifyLoggedIn() {
+	for (final Cookie cookie : CLIENT.getCookies().getCookies()) {
+	    if (cookie.getDomain().equals(Domain)) {
+		return true;
+	    }
 	}
 
-	private boolean verifyLoggedIn() {
-		for (Cookie cookie : CLIENT.getCookies().getCookies()) {
-			if (cookie.getDomain().equals(Domain)) {
-				return true;
-			}
-		}
+	return false;
+    }
 
-		return false;
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+
+	getSupportMenuInflater().inflate(R.menu.browser_menu, menu);
+
+	// TODO Add this to the menu xml
+	login = menu.add(isLoggedIn ? "Log out" : "Log in");
+	login.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+	// Get the SearchView and set the searchable configuration
+	final SearchManager searchManager = (SearchManager) this
+		.getSystemService(Context.SEARCH_SERVICE);
+	// Get the SearchView
+	final SearchView searchView = (SearchView) menu.findItem(
+		R.id.action_search).getActionView();
+
+	searchView.setSearchableInfo(searchManager.getSearchableInfo(this
+		.getComponentName()));
+	searchView.setIconifiedByDefault(false); // Do not iconify the widget;
+						 // expand it by default
+
+	// TODO Do something with the search
+	return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
+	if (item.equals(login)) {
+	    if (!isLoggedIn) {
+		login();
+	    } else
+		new LogoutTask(CLIENT, this).execute();
 	}
+	return super.onMenuItemSelected(featureId, item);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		login = menu.add(isLoggedIn ? "Log out" : "Log in");
-		login.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+    private void login() {
+	final LoginFragment login = new LoginFragment();
+	login.setLoginRequestListener(this);
+	login.show(getSupportFragmentManager(), "Login");
+    }
 
-		return super.onCreateOptionsMenu(menu);
+    @Override
+    public void onLoginFailed() {
+	setLoggedOut();
+    }
+
+    @Override
+    public void onLoggedIn() {
+	setLoggedIn();
+	loadFragment();
+    }
+
+    private void loadFragment() {
+	final FragmentTransaction ft = getSupportFragmentManager()
+		.beginTransaction();
+	// remove any fragments that are currently in loading screen.
+	final Fragment unneeded = getSupportFragmentManager().findFragmentById(
+		R.id.imageset_list);
+	if (unneeded != null && !unneeded.isDetached())
+	    ft.detach(unneeded);
+
+	final Bundle arguments = new Bundle();
+	// Activity created from search
+	String url;
+	if (searchQuery != null) {
+	    url = BASE_URL + "?f_search=" + searchQuery + "&page=";
+	} else
+	    url = BASE_URL + "?page=";
+	// DEBUG
+	Toast.makeText(this, "Search: " + url, Toast.LENGTH_LONG).show();
+
+	arguments.putString("url", url);
+
+	final ImageSetOverview setImageOverview = new ImageSetOverview();
+	setImageOverview.setArguments(arguments);
+	setImageOverview.setCallbackListener(this);
+	setImageOverview.setHasOptionsMenu(true);
+	if (mTwoPane)
+	    setImageOverview.setActivateOnItemClick(true);
+
+	ft.replace(R.id.imageset_list, setImageOverview);
+	ft.commit();
+    }
+
+    @Override
+    public void onLoginRequest(final String username, final String password) {
+	new LoginTask(CLIENT, this).execute(username, password);
+
+    }
+
+    @Override
+    public void onLoggedout() {
+	setLoggedOut();
+    }
+
+    @Override
+    public void onItemSelected(final ImageSetDescription id) {
+	if (mTwoPane) {
+	    // In two-pane mode, show the detail view in this activity by
+	    // adding or replacing the detail fragment using a
+	    // fragment transaction.
+	    final Bundle arguments = new Bundle();
+	    arguments.putParcelable(ImageSetDetailFragment.ARG_ITEM_ID, id);
+	    final ImageSetDetailFragment fragment = new ImageSetDetailFragment();
+	    fragment.setArguments(arguments);
+	    getSupportFragmentManager().beginTransaction()
+		    .replace(R.id.imageset_detail_container, fragment).commit();
+
+	} else {
+	    // In single-pane mode, simply start the detail activity
+	    // for the selected item ID.
+	    final Intent detailIntent = new Intent(this,
+		    ImageSetDetailActivity.class);
+	    detailIntent.putExtra(ImageSetDetailFragment.ARG_ITEM_ID, id);
+	    startActivity(detailIntent);
 	}
+    }
 
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		if (item.equals(login)) {
-			if (!isLoggedIn) {
-				login();
-			} else
-				new LogoutTask(CLIENT, this).execute();
-		}
-		return super.onMenuItemSelected(featureId, item);
-	}
+    private void setLoggedIn() {
+	isLoggedIn = true;
+	if (login != null)
+	    login.setTitle("Log out");
+    }
 
-	private void login() {
-		LoginFragment login = new LoginFragment();
-		login.setLoginRequestListener(this);
-		login.show(getSupportFragmentManager(), "Login");
-	}
+    private void setLoggedOut() {
+	getSupportFragmentManager().beginTransaction()
+		.replace(R.id.imageset_list, new SadPandaStubFragment())
+		.commit();
+	isLoggedIn = false;
+	if (login != null)
+	    login.setTitle("Log in");
+    }
 
-	@Override
-	public void onLoginFailed() {
-		setLoggedOut();
-	}
-
-	@Override
-	public void onLoggedIn() {
-		setLoggedIn();
-		loadFragment();
-	}
-
-	private void loadFragment() {
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		// remove any fragments that are currently in loading screen.
-		Fragment unneeded = getSupportFragmentManager().findFragmentById(
-				R.id.imageset_list);
-		if (unneeded != null && !unneeded.isDetached())
-			ft.detach(unneeded);
-
-		Bundle arguments = new Bundle();
-		arguments.putString("url", URL);
-
-		ImageSetOverview setImageOverview = new ImageSetOverview();
-		setImageOverview.setArguments(arguments);
-		setImageOverview.setCallbackListener(this);
-		setImageOverview.setHasOptionsMenu(true);
-		if (mTwoPane)
-			setImageOverview.setActivateOnItemClick(true);
-
-		ft.replace(R.id.imageset_list, setImageOverview);
-		ft.commit();
-	}
-
-	@Override
-	public void onLoginRequest(String username, String password) {
-		new LoginTask(CLIENT, this).execute(username, password);
-
-	}
-
-	@Override
-	public void onLoggedout() {
-		setLoggedOut();
-	}
-
-	@Override
-	public void onItemSelected(ImageSetDescription id) {
-		if (mTwoPane) {
-			// In two-pane mode, show the detail view in this activity by
-			// adding or replacing the detail fragment using a
-			// fragment transaction.
-			Bundle arguments = new Bundle();
-			arguments.putParcelable(ImageSetDetailFragment.ARG_ITEM_ID, id);
-			ImageSetDetailFragment fragment = new ImageSetDetailFragment();
-			fragment.setArguments(arguments);
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.imageset_detail_container, fragment).commit();
-
-		} else {
-			// In single-pane mode, simply start the detail activity
-			// for the selected item ID.
-			Intent detailIntent = new Intent(this, ImageSetDetailActivity.class);
-			detailIntent.putExtra(ImageSetDetailFragment.ARG_ITEM_ID, id);
-			startActivity(detailIntent);
-		}
-	}
-
-	private void setLoggedIn() {
-		isLoggedIn = true;
-		if (login != null)
-			login.setTitle("Log out");
-	}
-
-	private void setLoggedOut() {
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.imageset_list, new SadPandaStubFragment())
-				.commit();
-		isLoggedIn = false;
-		if (login != null)
-			login.setTitle("Log in");
-	}
-
-	public static Loader getImageLoader() {
-		return mManager.getLoader();
-	}
+    public static Loader getImageLoader() {
+	return mManager.getLoader();
+    }
 }
--- src/com/ecchi/sadpanda/overview/ImageSetOverview.java
+++ src/com/ecchi/sadpanda/overview/ImageSetOverview.java
@@ -22,175 +22,180 @@ import com.michaelnovakjr.numberpicker.NumberPickerDialog;
 import com.michaelnovakjr.numberpicker.NumberPickerDialog.OnNumberSetListener;
 
 public class ImageSetOverview extends SherlockFragment implements
-		OnNumberSetListener {
+	OnNumberSetListener {
 
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * activated item position. Only used on tablets.
+     */
+    private static final String STATE_ACTIVATED_POSITION = "activated_position";
+
+    /**
+     * The fragment's current callback object, which is notified of list item
+     * clicks.
+     */
+    private Callbacks mCallbacks = sDummyCallbacks;
+
+    /**
+     * The current activated item position. Only used on tablets.
+     */
+    private int mActivatedPosition = ListView.INVALID_POSITION;
+
+    /**
+     * A callback interface that all activities containing this fragment must
+     * implement. This mechanism allows activities to be notified of item
+     * selections.
+     */
+    public interface Callbacks {
 	/**
-	 * The serialization (saved instance state) Bundle key representing the
-	 * activated item position. Only used on tablets.
+	 * Callback for when an item has been selected.
 	 */
-	private static final String STATE_ACTIVATED_POSITION = "activated_position";
+	public void onItemSelected(ImageSetDescription id);
+    }
 
-	/**
-	 * The fragment's current callback object, which is notified of list item
-	 * clicks.
-	 */
-	private Callbacks mCallbacks = sDummyCallbacks;
-
-	/**
-	 * The current activated item position. Only used on tablets.
-	 */
-	private int mActivatedPosition = ListView.INVALID_POSITION;
-
-	/**
-	 * A callback interface that all activities containing this fragment must
-	 * implement. This mechanism allows activities to be notified of item
-	 * selections.
-	 */
-	public interface Callbacks {
-		/**
-		 * Callback for when an item has been selected.
-		 */
-		public void onItemSelected(ImageSetDescription id);
-	}
-
-	/**
-	 * A dummy implementation of the {@link Callbacks} interface that does
-	 * nothing. Used only when this fragment is not attached to an activity.
-	 */
-	private static Callbacks sDummyCallbacks = new Callbacks() {
-		@Override
-		public void onItemSelected(ImageSetDescription id) {
-		}
-	};
-
-	ListView mImageSetView;
-	ImageSetOverviewAdapter mImageSetAdapter;
-	boolean activateOnItemClick;
-
+    /**
+     * A dummy implementation of the {@link Callbacks} interface that does
+     * nothing. Used only when this fragment is not attached to an activity.
+     */
+    private static Callbacks sDummyCallbacks = new Callbacks() {
 	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
+	public void onItemSelected(final ImageSetDescription id) {
+	}
+    };
 
-		mImageSetAdapter = new ImageSetOverviewAdapter(getArguments()
-				.getString("url"), activity);
+    ListView mImageSetView;
+    ImageSetOverviewAdapter mImageSetAdapter;
+    boolean activateOnItemClick;
+
+    @Override
+    public void onAttach(final Activity activity) {
+	super.onAttach(activity);
+
+	mImageSetAdapter = new ImageSetOverviewAdapter(getArguments()
+		.getString("url"), activity);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+
+	mImageSetView.setAdapter(mImageSetAdapter);
+	mImageSetView.setOnScrollListener(mImageSetAdapter);
+
+	mImageSetView.setOnItemClickListener(new OnItemClickListener() {
+
+	    @Override
+	    public void onItemClick(final AdapterView<?> parent,
+		    final View view, final int position, final long id) {
+		if (mCallbacks != null)
+		    mCallbacks.onItemSelected(mImageSetAdapter
+			    .getItem(position));
+	    }
+	});
+
+	if (savedInstanceState != null
+		&& savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
+	    setActivatedPosition(savedInstanceState
+		    .getInt(STATE_ACTIVATED_POSITION));
 	}
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
+	setActivateOnItemClick(activateOnItemClick);
 
-		mImageSetView.setAdapter(mImageSetAdapter);
-		mImageSetView.setOnScrollListener(mImageSetAdapter);
+	super.onViewCreated(view, savedInstanceState);
+    }
 
-		mImageSetView.setOnItemClickListener(new OnItemClickListener() {
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+	    final ViewGroup container, final Bundle savedInstanceState) {
+	mImageSetView = new ListView(getActivity());
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				if (mCallbacks != null)
-					mCallbacks.onItemSelected(mImageSetAdapter
-							.getItem(position));
-			}
-		});
+	final ProgressBar emptyView = new ProgressBar(getActivity());
 
-		if (savedInstanceState != null
-				&& savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
-			setActivatedPosition(savedInstanceState
-					.getInt(STATE_ACTIVATED_POSITION));
-		}
+	final FrameLayout.LayoutParams emptyViewParams = new FrameLayout.LayoutParams(
+		80, 80, Gravity.CENTER);
 
-		setActivateOnItemClick(activateOnItemClick);
+	container.addView(emptyView, emptyViewParams);
 
-		super.onViewCreated(view, savedInstanceState);
+	mImageSetView.setEmptyView(emptyView);
+
+	return mImageSetView;
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+	super.onSaveInstanceState(outState);
+	if (mActivatedPosition != ListView.INVALID_POSITION) {
+	    // Serialize and persist the activated item position.
+	    outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
+	}
+    }
+
+    public void setCallbackListener(final Callbacks listener) {
+	mCallbacks = listener;
+    }
+
+    @Override
+    public void onDetach() {
+	super.onDetach();
+
+	mCallbacks = sDummyCallbacks;
+    }
+
+    /**
+     * Turns on activate-on-click mode. When this mode is on, list items will be
+     * given the 'activated' state when touched.
+     */
+    public void setActivateOnItemClick(final boolean activateOnItemClick) {
+	// When setting CHOICE_MODE_SINGLE, ListView will automatically
+	// give items the 'activated' state when touched.
+
+	this.activateOnItemClick = activateOnItemClick;
+
+	if (mImageSetView != null)
+	    mImageSetView
+		    .setChoiceMode(activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
+			    : ListView.CHOICE_MODE_NONE);
+    }
+
+    private void setActivatedPosition(final int position) {
+	if (position == ListView.INVALID_POSITION) {
+	    mImageSetView.setItemChecked(mActivatedPosition, false);
+	} else {
+	    mImageSetView.setItemChecked(position, true);
 	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		mImageSetView = new ListView(getActivity());
+	mActivatedPosition = position;
+    }
 
-		ProgressBar emptyView = new ProgressBar(getActivity());
+    @Override
+    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
+	inflater.inflate(R.menu.overview_menu, menu);
 
-		FrameLayout.LayoutParams emptyViewParams = new FrameLayout.LayoutParams(
-				80, 80, Gravity.CENTER);
+    }
 
-		container.addView(emptyView, emptyViewParams);
-
-		mImageSetView.setEmptyView(emptyView);
-
-		return mImageSetView;
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+	switch (item.getItemId()) {
+	case R.id.menu_refresh:
+	    mImageSetAdapter.clear(true);
+	    return true;
+	case R.id.go_to_page:
+	    final NumberPickerDialog dialog = new NumberPickerDialog(
+		    getActivity(), -1, mImageSetAdapter.getCurrentPage());
+	    dialog.setOnNumberSetListener(this);
+	    dialog.show();
+	    return true;
+	default:
+	    return super.onOptionsItemSelected(item);
 	}
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		if (mActivatedPosition != ListView.INVALID_POSITION) {
-			// Serialize and persist the activated item position.
-			outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
-		}
-	}
+    @Override
+    public void onNumberSet(final int selectedNumber) {
+	if (selectedNumber >= 0)
+	    mImageSetAdapter.goToPage(selectedNumber);
+    }
 
-	public void setCallbackListener(Callbacks listener) {
-		mCallbacks = listener;
-	}
-
-	@Override
-	public void onDetach() {
-		super.onDetach();
-
-		mCallbacks = sDummyCallbacks;
-	}
-
-	/**
-	 * Turns on activate-on-click mode. When this mode is on, list items will be
-	 * given the 'activated' state when touched.
-	 */
-	public void setActivateOnItemClick(boolean activateOnItemClick) {
-		// When setting CHOICE_MODE_SINGLE, ListView will automatically
-		// give items the 'activated' state when touched.
-
-		this.activateOnItemClick = activateOnItemClick;
-
-		if (mImageSetView != null)
-			mImageSetView
-					.setChoiceMode(activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
-							: ListView.CHOICE_MODE_NONE);
-	}
-
-	private void setActivatedPosition(int position) {
-		if (position == ListView.INVALID_POSITION) {
-			mImageSetView.setItemChecked(mActivatedPosition, false);
-		} else {
-			mImageSetView.setItemChecked(position, true);
-		}
-
-		mActivatedPosition = position;
-	}
-
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.overview_menu, menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_refresh:
-			mImageSetAdapter.clear(true);
-			return true;
-		case R.id.go_to_page:
-			NumberPickerDialog dialog = new NumberPickerDialog(getActivity(),
-					-1, mImageSetAdapter.getCurrentPage());
-			dialog.setOnNumberSetListener(this);
-			dialog.show();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	@Override
-	public void onNumberSet(int selectedNumber) {
-		if(selectedNumber >= 0)
-			mImageSetAdapter.goToPage(selectedNumber);
-	}
+    public void setUrl(final String url, final Activity activity) {
+	mImageSetAdapter = new ImageSetOverviewAdapter(url, activity);
+    }
 }
